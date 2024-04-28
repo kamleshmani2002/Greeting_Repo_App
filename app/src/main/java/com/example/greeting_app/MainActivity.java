80f2266b6462e9e66adc054a93b2f1b9b1d48858
Kamlesh mani Bhairam <papabhairamk205@gmail.com>
2024-04-28 22:02:47 +0530

    first commit

---
+++ app/src/main/java/com/example/greeting_app/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.greeting_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView = findViewById(R.id.textView2);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String InputName = editText.getText().toString();
+                Toast.makeText(MainActivity.this,
+                        "Welcome "+ InputName+" in our greeting App",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
